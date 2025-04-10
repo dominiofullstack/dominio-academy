cc42bbafadf62849b060a0234445ad0e8bdc92aa
dominiofullstack <dominiofullstack@gmail.com>
2025-04-09 23:47:37 -0300

    Header da tela de cursos

--- src/app/cursos-deslogado/cursos-deslogado.component.ts
+++ src/app/cursos-deslogado/cursos-deslogado.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../shared/navbar/navbar.component";
 import { TituloComDivisoresComponent } from "../shared/titulo-com-divisores/titulo-com-divisores.component";
+import { FormacaoListComponent } from "../home/formacao-list/formacao-list.component";
+import { ButtonDefaultComponent } from "../shared/button-default/button-default.component";
 
 @Component({
   selector: 'app-cursos-deslogado',
   standalone: true,
-  imports: [NavbarComponent, TituloComDivisoresComponent],
+  imports: [NavbarComponent, TituloComDivisoresComponent, FormacaoListComponent, ButtonDefaultComponent],
   templateUrl: './cursos-deslogado.component.html',
   styleUrl: './cursos-deslogado.component.scss'
 })
