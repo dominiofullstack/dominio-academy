720d2c130208c250ab07612721237b6ad595f4c1
dominiofullstack <dominiofullstack@gmail.com>
2025-05-01 23:30:37 -0300

    Novo componente para listagem de cursos

--- src/app/cursos-logado/cursos-logado.component.ts
+++ src/app/cursos-logado/cursos-logado.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarSearchComponent } from "../shared/navbar-search/navbar-search.component";
+import { ListaCursosComponent } from "./lista-cursos/lista-cursos.component";
 
 @Component({
   selector: 'app-cursos-logado',
   standalone: true,
-  imports: [NavbarSearchComponent],
+  imports: [NavbarSearchComponent, ListaCursosComponent],
   templateUrl: './cursos-logado.component.html',
   styleUrl: './cursos-logado.component.scss'
 })
