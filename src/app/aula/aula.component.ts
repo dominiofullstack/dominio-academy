45b7c726a08994119ea820ec0c55d3ee1b59380a
dominiofullstack <dominiofullstack@gmail.com>
2025-06-28 00:19:39 -0300

    Exibição da video aula

--- src/app/aula/aula.component.ts
+++ src/app/aula/aula.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ListaAulasComponent } from "./lista-aulas/lista-aulas.component";
+import { VideoAulaComponent } from "./video-aula/video-aula.component";
 
 @Component({
   selector: 'app-aula',
   standalone: true,
-  imports: [ListaAulasComponent],
+  imports: [ListaAulasComponent, VideoAulaComponent],
   templateUrl: './aula.component.html',
   styleUrl: './aula.component.scss'
 })
