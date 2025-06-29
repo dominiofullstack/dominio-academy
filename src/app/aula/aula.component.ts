12406d4422dd119a784b88b7b8658a5136d4cf12
dominiofullstack <dominiofullstack@gmail.com>
2025-06-29 14:13:30 -0300

    Tela de pergunta

--- src/app/aula/aula.component.ts
+++ src/app/aula/aula.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ListaAulasComponent } from "./lista-aulas/lista-aulas.component";
 import { VideoAulaComponent } from "./video-aula/video-aula.component";
+import { PerguntaComponent } from "./pergunta/pergunta.component";
 
 @Component({
   selector: 'app-aula',
   standalone: true,
-  imports: [ListaAulasComponent, VideoAulaComponent],
+  imports: [ListaAulasComponent, VideoAulaComponent, PerguntaComponent],
   templateUrl: './aula.component.html',
   styleUrl: './aula.component.scss'
 })
